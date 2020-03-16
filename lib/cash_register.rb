239ec9f585b388b03f132c124af2145af717e8d7
Hassan Bajwa <bajwahassan3@outlook.com>
2020-03-16 11:11:40 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,7 @@ class CashRegister
     if @discount = 0 
       puts "NO discount to apply"
     else 
-    @total = @total - (@total * (@discount/100))
+    @total = @total - (@total * (@discount/100)).to_i
     end
   end
     
