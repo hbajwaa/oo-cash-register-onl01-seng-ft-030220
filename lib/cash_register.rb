936af73bfbd81a308ec8568e7d3075817b8ba032
Hassan Bajwa <bajwahassan3@outlook.com>
2020-03-16 11:20:40 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister
-  attr_accessor :total, :discount, :quantity
-  
+  attr_accessor :total, :discount, :quantity :items
+  @@items = []
   def initialize(discount =0)
     @total = 0
     @discount = discount
